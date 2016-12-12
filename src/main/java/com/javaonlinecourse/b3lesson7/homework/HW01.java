f73b1462bc6afcf6949159d792e88025418ae6d4
E_Mitrohin <emitrohin@icloud.com>
2016-12-12 14:15:19 +0500

    b3lesson7 home work

---
+++ src/main/java/com/javaonlinecourse/b3lesson7/homework/HW01.java
@@ -0,0 +1,11 @@
+package com.javaonlinecourse.b3lesson7.homework;
+
+/**
+ * Author: E_Mitrohin
+ * Date:   12.12.2016.
+ *
+ * Создать public static class MyThread с помощью интерфейса Runnable.
+ * MyThread должен выводить "Это мой первый Thread class".
+ */
+public class HW01 {
+}
